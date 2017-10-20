e0c488c776c267e362b6891a1f82ee8bcb2e3e91
yeshwantkumar <yeshwantit065@gmail.com>
2017-10-20 13:04:16 +0530

    Demo of ionScroll not firing, when we use ion-slides

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  currentSegment: string = "segment1";  
+  @ViewChild(Slides) slides: Slides;
+  
   constructor(public navCtrl: NavController) {
 
+  }
+
+  doSomething(event){
+    console.log("Scroll fired");
+  }
+
+  showSegment() {
+    if (this.currentSegment == "segment1") {
+      this.slides.slideTo(0, 0);
+    }
+    else if (this.currentSegment == "segment2") {
+      this.slides.slideTo(1, 0);
+    }
+  }
+
+  slideChanged() {
+    if (this.slides._activeIndex == 0) {
+      this.currentSegment = "segment1";
+    }
+    else if (this.slides._activeIndex == 1) {
+      this.currentSegment = "segment2";
+    }
   }
 
 }
